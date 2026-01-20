7cb5300ee0d46ef1ef2b34f8a21e1d51aa712720
Pulkit <choprapulkitt@gmail.com>
2026-01-20 23:09:14 +0530

    Email Requirement

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,26 +1,29 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
 
-const userschema = new Schema({
-    name : {
-       type : String,
-       required : true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email :{ 
-        type :String
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    profileImage:{
-        type:String,
-        default:""
+    profileImage: {
+      type: String,
+      default: "",
     },
-    clerkId:{
-        type :String,
-        required : true,
-        unique:true
-    }
-   
-},{timestamps:true})
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
 
- const User = mongoose.model("User",userschema)
- export default User;
+const User = mongoose.model("User", userSchema);
 
-
+export default User;
